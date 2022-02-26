7e1e941c1a6ee9eac8b50db271e7be73e248d5fb
obeaune <linecora86@gmail.com>
2022-02-25 22:59:03 -0300

    Testando o componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/url-desconhecida');
+    const notFoundHeadingEl = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    const emojiFromHeading = screen.getByLabelText('Crying emoji');
+    expect(notFoundHeadingEl && emojiFromHeading).toBeInTheDocument();
+  });
+
+  test('Se a página mostra a imagem do pikachu chorando', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/url-desconhecida');
+    const imgPikachu = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imgPikachu).toHaveAttribute('src', imgURL);
+  });
+});
